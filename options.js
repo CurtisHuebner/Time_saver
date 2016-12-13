241c19038f05dd057f94ff32e0f30da1c5bc7daa
Curtis Huebner <mathcurt@gmail.com>
2016-12-13 11:45:26 -0800

    added warning when invalid JSON is entered.

--- options.js
+++ options.js
@@ -1,27 +1,33 @@
 // Saves options to chrome.storage
 function save_options() {
-  var textData = document.getElementById('data').value;
-  var data = {"data":JSON.parse(textData)};
-  chrome.storage.sync.set(data, function() {
-    // Update status to let user know options were saved.
+    var textData = document.getElementById('data').value;
     var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    try {
+        var data = {"data":JSON.parse(textData)};
+    }
+    catch(err){
+        //Notify the user that the entered JSON is invalid
+        status.textContent = 'Invalid JSON!';
+    }
+    chrome.storage.sync.set(data, function() {
+        // Update status to let user know options were saved.
+        status.textContent = 'Options saved.';
+    });
     setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
-  var bkp = chrome.extension.getBackgroundPage()
-  bkp.updateData();
+        status.textContent = '';
+    }, 1150);
+    var bkp = chrome.extension.getBackgroundPage()
+    bkp.updateData();
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  chrome.storage.sync.get("data", function(data) {
-    jsonString = JSON.stringify(data["data"],null,'\t');
-    //console.log(jsonString);
-    document.getElementById('data').value = jsonString;
-  });
+    chrome.storage.sync.get("data", function(data) {
+        jsonString = JSON.stringify(data["data"],null,'\t');
+        //console.log(jsonString);
+        document.getElementById('data').value = jsonString;
+    });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
